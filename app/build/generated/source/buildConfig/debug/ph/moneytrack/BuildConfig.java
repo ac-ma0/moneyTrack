6201f25bf1e07b7a1844e7b639a898859a1d55b3
ac-ma0 <alvin.c.m.abamonga>
2026-09-16 20:34:44 +0000

    rrrr

---
+++ app/build/generated/source/buildConfig/debug/ph/moneytrack/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ph.moneytrack;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ph.moneytrack";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String SUPABASE_ANON_KEY = "sb_publishable_-LEQQFtT6qMcMHs6fDlIJA_wSpyIENU";
+  public static final String SUPABASE_URL = "https://rkwgxsxepldzvbajtlgg.supabase.co";
+}
